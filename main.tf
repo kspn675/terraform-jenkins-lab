bd83b91078254c352e597eb5ec06781c955ef339
kspn675 <116645336+kspn675@users.noreply.github.com>
2026-01-14 15:58:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,25 +7,23 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "kas-rg"
-    storage_account_name = "tfstatestorage675"
-    container_name       = "terraform-state"
-    key                  = "jenkins-demo.tfstate"
+    resource_group_name  = "tfstate-terraform-adi"
+    storage_account_name = "tfstorageadi"
+    container_name       = "adicontainerterraform-state"
+    key                  = "adi-jenkins-demo.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
-  
 }
-
 resource "azurerm_resource_group" "rg" {
-  name     = "JenkinsTerraformRG"
-  location = "East US"
+  name     = "adi-JenkinsTerraformRG"
+  location = "West US"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "JenkinsVNet"
+  name                = "adi-JenkinsVNet"
   address_space       = ["10.10.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
